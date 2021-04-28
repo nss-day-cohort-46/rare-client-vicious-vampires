bfb4eb26d339b1007ca5bf9e427901ade6e259a1
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-04-28 15:49:02 -0500

    Added Nav Links

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -12,8 +12,21 @@ export const NavBar = () => {
                 <img className="navbar__logo" src={Logo} />
             </li>
             <li className="navbar__item">
-                <Link className="navbar__link" to="/">Posts</Link>
+                <Link className="navbar__link" to="/">Home</Link>
             </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/categories">Category Management</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/comments">Add Comment</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/tags">Tag Management</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/posts">My Posts</Link>
+            </li>
+
             {
                 (localStorage.getItem("rare_user_id") !== null) ?
                     <li className="nav-item">
@@ -31,6 +44,7 @@ export const NavBar = () => {
                         <li className="nav-item">
                             <Link className="nav-link" to="/register">Register</Link>
                         </li>
+
                     </>
             }        </ul>
     )
